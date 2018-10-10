7737edc77541ae8292a36538b4058a0cf28e003f
ahmed1 <ahmed1@rose-hulman.edu>
2018-10-09 20:39:45 -0400

    problem(s) solved

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 2, problem 2.
 
 Authors: Dave Fisher, David Mutchler, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Tom Ahmed.  October 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
@@ -109,8 +109,8 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem2a()
-    # run_test_problem2b()
+    run_test_problem2a()
+    run_test_problem2b()
 
 
 def run_test_problem2a():
@@ -196,9 +196,11 @@ def problem2a(triangle):
       :rtype: Triangle
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    new_triangle = Triangle(triangle.a*2, triangle.b*2, triangle.c*2)
+    return new_triangle
 
 
 def run_test_problem2b():
@@ -354,10 +356,16 @@ def problem2b(triangles):
       :rtype: int | float
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: See the HINT just before the DEF of this function.
     # -------------------------------------------------------------------------
+    sum_areas = 0
+    if len(triangles) == 0:
+        return 0
+    for i in range(len(triangles)):
+        sum_areas += triangles[i].get_area()
+    return sum_areas
 
 
 ###############################################################################
